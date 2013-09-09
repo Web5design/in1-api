5699ce2709b6a3348c68833bb5c73643b844896f
Carol Skelly <carol@iatek.com>
2013-09-08 20:31:35 -0400

    first commit

--- site.js
+++ site.js
@@ -2,7 +2,8 @@ var request = require('request'),
      conf = require('./conf');
 
 exports.sourcesMw = function(cb){
-    request.get({url:'https://api.parse.com/1/classes/Source',json:true,limit:200,order:"-createdAt",headers:{'X-Parse-Application-Id':conf.parse.appKey,'X-Parse-REST-API-Key':conf.parse.restKey}},function(e,r,b){
+    var whereClause = {"type":{"$ne":"hashtag"}};
+    request.get({url:'https://api.parse.com/1/classes/Source',json:true,qs:{where:JSON.stringify(whereClause)},limit:200,order:"-createdAt",headers:{'X-Parse-Application-Id':conf.parse.appKey,'X-Parse-REST-API-Key':conf.parse.restKey}},function(e,r,b){
         cb(b.results);
     });
 };
