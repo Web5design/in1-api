d24e18dd181c60fa0462bc665ee0c1b216bd7d3d
Carol Skelly <carol@iatek.com>
2013-09-22 19:38:16 -0400

    beta

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ exports.init = function(port) {
         app.use(express.methodOverride());
         app.use(express.cookieParser());
         app.use(express.cookieSession({cookie:{path:'/',httpOnly:true,maxAge:null},secret:'skeletor'}));
+        app.use(app.router);
         app.use(everyauth.middleware(app));
        
-        app.use(app.router);
+        
     });
 
     app.engine('ejs', engine);
